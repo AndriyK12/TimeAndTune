39b42cd6b267f1a2fe06677d6f9b6fd346d32e7a
AndriyK12 <124818943+AndriyK12@users.noreply.github.com>
2023-11-01 12:13:06 +0200

    Added a project wuth DAL

---
+++ Program/TimeAndTune/EFCore/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCore
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (var context = new TTContext())
+            {
+                var allUsers = context.Users.ToList();
+                var allTasks = context.Tasks.ToList();
+                var allSounds = context.Sounds.ToList();
+
+                var firstTaskUsername = allTasks[0].UseridrefNavigation?.Username;
+                var secondTaskUsername = allTasks[1].UseridrefNavigation?.Username;
+                Console.WriteLine("Username of the user with the first task: " + firstTaskUsername);
+                Console.WriteLine("Username of the user with the second task: " + secondTaskUsername);
+
+                foreach (var user in allUsers)
+                {
+                    Console.WriteLine("===================================================================================");
+                    Console.WriteLine($" User ID: {user.Userid},\n Username: {user.Username},\n Email {user.Email},\n Coins amout: {user.Coinsamount},\n Password: {user.Password}");
+                    Console.WriteLine("===================================================================================");
+                }
+                foreach (var task in allTasks)
+                {
+                    Console.WriteLine("===================================================================================");
+                    Console.WriteLine($" Task ID: {task.Taskid},\n UserRef: {task.Useridref},\n Name {task.Name},\n Description: {task.Description},\n Priority: {task.Priority},\n Completed status: {task.Completed},\n Date of creation: {task.Dateofcreation},\n Expected finish time{task.Expectedfinishtime},\n Finish time {task.Finishtime},\n Execution time: {task.Executiontime}");
+                    Console.WriteLine("===================================================================================");
+                }
+                foreach (var sound in allSounds)
+                {
+                    Console.WriteLine("===================================================================================");
+                    Console.WriteLine($" Sound ID: {sound.Soundid},\n Sound Name: {sound.Soundname}");
+                    Console.WriteLine("===================================================================================");
+                }
+            }
+        }
+    }
+}
---
+++ Program/TimeAndTune/EFCore/Sound.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCore;
+
+public partial class Sound
+{
+    public int Soundid { get; set; }
+
+    public string Soundname { get; set; } = null!;
+
+    public string Audiofilepath { get; set; } = null!;
+
+    public string Photofilepath { get; set; } = null!;
+}
