55fd63fd3849d15dac00459fb2ab3e14a0da917c
AndriyK12 <124818943+AndriyK12@users.noreply.github.com>
2023-11-15 18:23:50 +0200

    added automaticall histogram

--- Program/TimeAndTune/TimeAndTune/Pages/StatisticsPage.xaml.cs
+++ Program/TimeAndTune/TimeAndTune/Pages/StatisticsPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +23,8 @@ namespace TimeAndTune
     /// </summary>
     public partial class StatisticsPage : Page
     {
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
         public void openNavigation_Click(object sender, RoutedEventArgs e)
         {
             NavWindow nav = new NavWindow();
@@ -68,6 +73,37 @@ namespace TimeAndTune
         public StatisticsPage()
         {
             InitializeComponent();
+            var dates = new List<DateTime>();
+            var completedTasks = new List<int>();
+
+            for (int i = 6; i >= 0; i--)
+            {
+                dates.Add(DateTime.Now.AddDays(-i)); // Додаємо дати в зворотньому порядку
+                completedTasks.Add(i + 1); // Кількість завдань від 1 до 7
+            }
+
+            var gradientBrush = new LinearGradientBrush
+            {
+                StartPoint = new Point(0, 0),
+                EndPoint = new Point(0, 1)
+            };
+            gradientBrush.GradientStops.Add(new GradientStop(Color.FromArgb(0xFF, 0x7F, 0x22, 0xAB), 0));
+            gradientBrush.GradientStops.Add(new GradientStop(Color.FromArgb(0xFF, 0x6D, 0x67, 0x70), 1));
+
+            // Побудова гістограми
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "Completed Tasks",
+                    Values = new ChartValues<int>(completedTasks),
+                    Fill = gradientBrush,
+                }
+            };
+
+            Labels = dates.Select(date => date.ToString("dd/MM")).ToArray();
+
+            DataContext = this;
         }
     }
 }
